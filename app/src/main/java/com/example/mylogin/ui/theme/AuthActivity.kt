544a8597458206bf6f59d6834686b6b351fa74f6
carlitos06092001 <ingcarlosdelgado.info@gmail.com>
2024-03-24 06:34:44 -0400

    Initial commit

---
+++ app/src/main/java/com/example/mylogin/ui/theme/AuthActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mylogin.ui.theme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mylogin.R
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
